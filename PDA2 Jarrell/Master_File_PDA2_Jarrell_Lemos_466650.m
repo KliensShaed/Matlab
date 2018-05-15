6d3746dc882eff9cc76db0c7563bd49d66269d32
Jarrell Lemos <38882439+KliensShaed@users.noreply.github.com>
2018-05-15 19:48:41 +1000

    main

---
+++ PDA2 Jarrell/Master_File_PDA2_Jarrell_Lemos_466650.m
@@ -0,0 +1,31 @@
+%% PDA 2   
+% Surname: Lemos
+% First Name: Jarrell
+% Student Number: 466650
+
+%%
+clear
+close all
+clc
+
+%% Part 1
+% Phase Averaging
+% 
+f = 0.8;
+Part_1
+
+%% Part 2
+% Description
+Part_2
+
+%% Part 3
+% Description blablabla
+clear
+close all
+clc
+
+Exp_filename='Run36-02_moving.dat';
+f=0.8;
+
+Part_3
+
---
+++ PDA2 Jarrell/PhaseAverage.m
@@ -0,0 +1,6 @@
+function [Tvec, Data_PhAv] = PhaseAverage(t,Data,f)
+T = 1./f;
+Tvec = linspace(0, T, 51);
+Data = (Phase_WP)+(OWC_WP)./2
+Data_PhAv;
+end
